c510c55eba26c0e8b759dee6cdbc026cdaa7f009
Felipe <felipesakaguti@hotmail.com>
2022-06-06 17:01:30 -0300

    Initial Commit / Created header

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import { Header } from './Components/Header';
+import './global.css';
+
+export function App() {
+
+  return (
+    <>
+      <Header/>
+      <h1>Hello World</h1>
+    </>
+
+  )
+}
